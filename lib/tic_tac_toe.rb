a98e267919454f9a406d2d14e70d66c77030e5a1
Michael Soares <mikey.soares@gmail.com>
2018-11-02 15:30:12 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -32,7 +32,7 @@ def input_to_index(input)
     end
   end
 
-def move(board, index, current_player)
+def move(board, index, current_player = "X")
   @board[index] = current_player
   end
 def position_taken?(board, index)
