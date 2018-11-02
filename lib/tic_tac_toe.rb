dca307f0d3ba3447dc0d4f3ee8c253fc1f641ef7
Michael Soares <mikey.soares@gmail.com>
2018-11-02 15:14:13 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,5 +1,22 @@
 class TicTacToe
-def intialize(board)
+def intialize(board = nil)
   @board = board == Array.new(9," ")
-end
+  end
+WIN_COMBINATIONS = [
+  [0, 1, 2], #top row
+  [3, 4, 5], #middle row
+  [6, 7, 8], #bottom row
+  [0, 3, 6], #left column
+  [1, 4, 7], #middle column
+  [2, 5, 8], #right column
+  [0, 4, 8], #topleft to botright diag
+  [2, 4, 6] #topright to botleft diag
+]
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  end
 end
